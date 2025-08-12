93d866568092e019b6b5c5eb87b8756a405551cf
MINJI <always0524@naver.com>
2025-08-13 00:05:11 +0900

    [Feat] 01-Practice - avatar 컴포넌트 생성

---
+++ 01-practice/src/components/avatar.js
@@ -0,0 +1,11 @@
+// import profile from '../../assets/images/minji-profile.png'
+
+export default function Aavatar() {
+  return React.createElement("img", {
+    src: "/01-practice/assets/images/minji-profile.png",
+    alt: "프로필사진",
+    className: "profile",
+    width: "40px",
+    height: "40px",
+  });
+}
