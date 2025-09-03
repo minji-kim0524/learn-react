c65adef1a170d118ab46b33721afdd60b0198d95
MINJI <always0524@naver.com>
2025-09-04 08:22:40 +0900

    [Learn] 03-practice - DOMRef 값 참조 - ref callback - useRef + useCallback

--- 03-practice/src/app.jsx
+++ 03-practice/src/app.jsx
@@ -1,10 +1,93 @@
+import { useEffect, useRef, useState } from 'react'
 import { LearnSection } from '@/components'
-import RandomCountUp from '@/demo/app'
 
 export default function App() {
+  const [attach, setAttach] = useState(true)
+
+  // 1. ref callback
+  // const refCallback = (el) => {
+  //   el?.setAttribute('tabindex', '-1')
+  //   el?.focus()
+
+  //   const intervalId = setInterval(() => {
+  //     console.log(new Date().toLocaleDateString())
+  //   }, 1000)
+
+  //   return () => {
+  //     clearInterval(intervalId)
+  //   }
+  // }
+
+  // 2. useRef + useCallback
+  const pRef = useRef(null)
+  const intervalRef = useRef()
+
+  useEffect(() => {
+    const pElement = pRef.current
+
+    pElement?.setAttribute('tabindex', '-1')
+    pElement?.focus()
+
+    intervalRef.current = setInterval(() => {
+      console.log(new Date().toLocaleDateString())
+    }, 1000)
+
+    return () => {
+      clearInterval(intervalRef.current)
+    }
+  }, [attach])
+
   return (
-    <LearnSection title="랜덤 카운트 업">
-      <RandomCountUp />
+    <LearnSection title="DOM 참조">
+      <Demo />
+      <div className="paragraphes space-y-2 [&_p]:text-gray-700 [&_p]:font-semibold">
+        {attach && (
+          <div className="bg-amber-300 p-5 pt-2.5 my-2">
+            <p
+              ref={pRef}
+              className="focus:outline-8 outline-offset-2 outline-blue-500/40"
+            >
+              one
+            </p>
+          </div>
+        )}
+        <button
+          className="button mt-2"
+          onClick={() => {
+            setAttach((a) => !a)
+            clearInterval(intervalRef.current)
+          }}
+        >
+          토글
+        </button>
+        <p>two</p>
+        <p>three</p>
+      </div>
     </LearnSection>
+  )
+}
+
+function Demo() {
+  useEffect(() => {
+    const pElements = document.querySelectorAll('.paragraphes')
+    console.log(pElements)
+  })
+
+  const articleRef = useRef(null)
+  const divRef = useRef(null)
+
+  useEffect(() => {
+    console.log(articleRef.current)
+    console.log(divRef.current)
+  })
+
+  return (
+    <article ref={articleRef}>
+      <h2>아티클</h2>
+      <div ref={divRef} className="paragraphes"></div>
+      <p>hello</p>
+      <p>World</p>
+      <p>welcome</p>
+    </article>
   )
 }
