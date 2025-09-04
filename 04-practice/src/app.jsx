7603b5b26564293bbbcfa3bf407629cdf3633222
MINJI <always0524@naver.com>
2025-09-05 01:36:32 +0900

    [Learn] 04-practice - 렌더링 영역에 부수효과 코드가 포함된 경우

--- 04-practice/src/app.jsx
+++ 04-practice/src/app.jsx
@@ -1,10 +1,29 @@
+import { useEffect, useState } from 'react'
 import { LearnSection } from '@/components'
-import RandomCountUp from '@/demo/app'
 
 export default function App() {
+  console.log('App 렌더링')
+
+  const [key, setKey] = useState(0)
+
+  // 데이터 가져오기 상태관리
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    setLoading(true)
+    setError(null)
+  })
+
+  fetch('https://jsonplaceholder.typicode.com/albums/1')
+    .then((response) => response.json())
+    .then(console.log)
+    .catch(console.error)
+
   return (
-    <LearnSection title="랜덤 카운트 업">
-      <RandomCountUp />
+    <LearnSection title="데이터 가져오기(fetching data)" showTitle>
+      <p>{'앨범이름'}</p>
     </LearnSection>
   )
 }
