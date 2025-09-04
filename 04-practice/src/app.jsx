da6cd81c02b721261621e2326919ba4298b59999
MINJI <always0524@naver.com>
2025-09-05 02:12:32 +0900

    [Learn] 04-practice - fetching data 상태관리

--- 04-practice/src/app.jsx
+++ 04-practice/src/app.jsx
@@ -14,16 +14,36 @@ export default function App() {
   useEffect(() => {
     setLoading(true)
     setError(null)
-  })
 
-  fetch('https://jsonplaceholder.typicode.com/albums/1')
-    .then((response) => response.json())
-    .then(console.log)
-    .catch(console.error)
+    fetch('https://jsonplaceholder.typicode.com/albums/1')
+      .then((response) => response.json())
+      .then((responseData) => {
+        setData(responseData)
+
+        setLoading(false)
+      })
+      .catch((error) => {
+        setError(error)
+
+        setLoading(false)
+      })
+  }, [])
+
+  console.log({ loading, error, data })
 
   return (
     <LearnSection title="데이터 가져오기(fetching data)" showTitle>
       <p>{'앨범이름'}</p>
+      <div role="group" className="mt-5">
+        <button
+          type="button"
+          className="button"
+          onClick={() => setKey((k) => k + 1)}
+        >
+          렌더링 키 변경
+        </button>
+        <output>렌더링 키: {key}</output>
+      </div>
     </LearnSection>
   )
 }
