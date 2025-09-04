e78095bd8ca93f1e101651008e2ff8f4571ec90b
MINJI <always0524@naver.com>
2025-09-05 04:38:35 +0900

    [Learn] 04-practice - 조건부 렌더링

--- 04-practice/src/app.jsx
+++ 04-practice/src/app.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from 'react'
 import { LearnSection } from '@/components'
+import { wait } from './utils'
+
+const API_URL = 'https://jsonplaceholder.typicode.com/albums/100'
 
 export default function App() {
   console.log('App 렌더링')
@@ -15,25 +18,64 @@ export default function App() {
     setLoading(true)
     setError(null)
 
-    fetch('https://jsonplaceholder.typicode.com/albums/1')
-      .then((response) => response.json())
+    fetch(API_URL)
+      .then(async (response) => {
+        await wait(2)
+
+        if (response.ok) {
+          return response.json()
+        }
+
+        if (response.status === 404) {
+          throw new Error('응답 데이터를 찾을 수 없습니다.')
+        }
+      })
       .then((responseData) => {
         setData(responseData)
-
-        setLoading(false)
       })
       .catch((error) => {
         setError(error)
-
+      })
+      .finally(() => {
         setLoading(false)
       })
-  }, [])
+  }, [key])
 
   console.log({ loading, error, data })
 
+  let renderElement = null
+
+  if (loading) {
+    renderElement = (
+      <p
+        role="status"
+        aria-live="polite"
+        className="text-indigo-300 font-semibold text 2x1"
+      >
+        로딩 중...
+      </p>
+    )
+  } else if (error) {
+    renderElement = (
+      <p
+        role="alert"
+        aria-live="assertive"
+        className="text-red-600 font-semibold text-2x1"
+      >
+        오류 발생!! {error.message}
+      </p>
+    )
+  } else {
+    renderElement = (
+      <p>
+        앨범 타이틀: {data?.id ?? 0} | {data?.title ?? 'Album Title'}
+      </p>
+    )
+  }
+
   return (
     <LearnSection title="데이터 가져오기(fetching data)" showTitle>
-      <p>{'앨범이름'}</p>
+      {renderElement}
       <div role="group" className="mt-5">
         <button
           type="button"
