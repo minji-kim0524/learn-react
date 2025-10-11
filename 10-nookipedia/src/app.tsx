e7aff4660877ccb98e0123a3819cbeedbecf7b97
MINJI <always0524@naver.com>
2025-10-11 23:43:05 +0900

    [Feat] 10-nookipedia 실습 - 데이터 가져오기

--- 10-nookipedia/src/app.tsx
+++ 10-nookipedia/src/app.tsx
@@ -1,9 +1,36 @@
-import { LearnSection, RandomCountUp } from '@/components'
+import { useEffect, useState } from 'react'
+
+const url = import.meta.env.VITE_URL
+const API_KEY = import.meta.env.VITE_API_KEY
 
 export default function App() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    async function get() {
+      const response = await fetch(url, {
+        headers: {
+          'Content-Type': 'application/json',
+          'X-API-Key': API_KEY,
+        },
+      })
+
+      console.log(response)
+      const responseData = await response.json()
+      console.log(responseData)
+      setData(responseData)
+    }
+    get()
+  }, [])
+
   return (
-    <LearnSection title="랜덤 카운트 업">
-      <RandomCountUp />
-    </LearnSection>
+    <div>
+      {data.map((item) => (
+        <>
+          <p>{item.name}</p>
+          <img src={item.image_url} alt="사진" width={60} height={70} />
+        </>
+      ))}
+    </div>
   )
 }
