ac1e3bc672d7e8699e595ab635d698f766f4d4e3
MINJI <always0524@naver.com>
2026-01-14 23:09:03 +0900

    [Feat] 10-nookipedia 실습 - input 요소에 검색어 입력시 오디오 끊김현상 보완

--- 10-nookipedia/src/components/Input.tsx
+++ 10-nookipedia/src/components/Input.tsx
@@ -1,6 +1,17 @@
-export default function Input({ id }: { id: string }) {
+import React from 'react'
+
+type InputProps = {
+  id: string
+  //   onSearch: () => void
+}
+
+export default function Input({ id }: InputProps) {
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+  }
+
   return (
-    <form className="flex flex-row gap-2 mx-auto my-4">
+    <form className="flex flex-row gap-2 mx-auto my-4" onSubmit={handleSubmit}>
       <label htmlFor={id} className="sr-only">
         검색창
       </label>
@@ -10,7 +21,9 @@ export default function Input({ id }: { id: string }) {
         placeholder="검색어를 입력해주세요"
         className="border-1 rounded-sm p-2"
       />
-      <button className="border-1 rounded-sm p-2">Search</button>
+      <button type="submit" className="border-1 rounded-sm p-2">
+        Search
+      </button>
     </form>
   )
 }
