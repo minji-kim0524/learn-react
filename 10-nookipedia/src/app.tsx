0fe8a9623f0500b6a33613821265aa447b8ecaa5
MINJI <always0524@naver.com>
2026-01-16 16:05:46 +0900

    [Feat] 10-nookipedia 실습 - 검색기능 구현

--- 10-nookipedia/src/app.tsx
+++ 10-nookipedia/src/app.tsx
@@ -2,6 +2,7 @@ import audioFile from '/public/navi_song.mp3'
 import { useEffect, useRef, useState } from 'react'
 import { Volume2, VolumeOff } from 'lucide-react'
 import { CharList } from './@types/global'
+import Button from './components/Button'
 import Input from './components/Input'
 import Modal from './components/Modal'
 
@@ -20,6 +21,9 @@ export default function App() {
   const [selectedItem, setSelectedItem] = useState<CharacterInfo>()
   const audioRef = useRef<HTMLAudioElement | null>(null)
   const [isPlaying, setIsPlaying] = useState(false)
+  const [query, setQuery] = useState('')
+  const [filteredData, setFilteredData] = useState<CharacterInfo[]>([])
+  const [isSearched, setIsSearched] = useState(false)
 
   function handlePlayPause() {
     if (!audioRef.current) {
@@ -33,6 +37,25 @@ export default function App() {
     }
 
     setIsPlaying((prev) => !prev)
+  }
+
+  function handleSearch() {
+    const searchedData = data.filter((v) =>
+      v.name.toLowerCase().includes(query.toLowerCase())
+    )
+
+    console.log(searchedData)
+    setFilteredData(searchedData)
+    setIsSearched(true)
+
+    setQuery('')
+  }
+
+  function handleBackHome() {
+    setQuery('')
+    setFilteredData([])
+    setIsSearched(false)
+    // setSelectedItem()
   }
 
   useEffect(() => {
@@ -51,6 +74,8 @@ export default function App() {
     }
     get()
   }, [])
+
+  const responseData = isSearched ? filteredData : data
 
   return (
     <section className="p-2">
@@ -72,16 +97,24 @@ export default function App() {
           onClose={() => setIsOpen(false)}
         />
       )}
-      <img
-        src="/footer_navi_logo.png"
-        alt="로고"
-        className="flex mx-auto mb-2"
-      />
+      <button
+        type="button"
+        onClick={handleBackHome}
+        className="flex mx-auto mb-2 bg-transparent border-none"
+      >
+        <img src="/footer_navi_logo.png" alt="로고" />
+      </button>
       <div className="flex flex-row gap-1 mb-2">
-        <Input id="name" />
+        <Input
+          id="name"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          onSubmit={handleSearch}
+        />
+        {/* <Button /> */}
       </div>
       <div className="flex flex-wrap gap-6 px-2">
-        {data.map((item) => (
+        {responseData.map((item) => (
           <div className="flex flex-col items-center cursor-pointer hover:scale-120 hover:transition-transform">
             <button
               type="button"
--- 10-nookipedia/src/components/Input.tsx
+++ 10-nookipedia/src/components/Input.tsx
@@ -2,22 +2,32 @@ import React from 'react'
 
 type InputProps = {
   id: string
-  //   onSearch: () => void
-}
+  value: string
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  onSubmit: () => void
+} & React.InputHTMLAttributes<HTMLInputElement>
 
-export default function Input({ id }: InputProps) {
-  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault()
-  }
+export default function Input({ id, value, onChange, onSubmit }: InputProps) {
+  // function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+  //   e.preventDefault()
+  // }
 
   return (
-    <form className="flex flex-row gap-2 mx-auto my-4" onSubmit={handleSubmit}>
+    <form
+      className="flex flex-row gap-2 mx-auto my-4"
+      onSubmit={(e) => {
+        e.preventDefault()
+        onSubmit()
+      }}
+    >
       <label htmlFor={id} className="sr-only">
         검색창
       </label>
       <input
         id={id}
         type="text"
+        value={value}
+        onChange={onChange}
         placeholder="검색어를 입력해주세요"
         className="border-1 rounded-sm p-2"
       />
