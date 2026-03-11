be4def5c056cc536110ab18f3e9ee9f6e793a12a
MINJI <always0524@naver.com>
2026-03-11 20:48:19 +0900

    [Style] 10-nookipedia 실습 - 월별 조회기능 버튼 hover시 눌림효과 적용

--- 10-nookipedia/src/components/FilterButton.tsx
+++ 10-nookipedia/src/components/FilterButton.tsx
@@ -7,13 +7,14 @@ interface FilterButtonProps
 
 export default function FilterButton({
   birthButton,
+  className,
   ...props
 }: FilterButtonProps) {
   return (
     <>
       <button
         {...props}
-        className="text-2xl font-semibold text-[#614f34] bg-white px-4 py-1 rounded-3xl border-0"
+        className={`text-2xl font-semibold text-[#614f34] px-4 py-1 rounded-3xl border-0 ${className ?? ''}`}
       >
         {birthButton}
       </button>
