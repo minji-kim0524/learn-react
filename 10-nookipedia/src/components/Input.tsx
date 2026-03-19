dae974f73444b302c56d772aa49f27db59c2b540
MINJI <always0524@naver.com>
2026-03-19 22:22:27 +0900

    [Style] 10-nookipedia 실습 - 검색버튼 스타일 변경

--- 10-nookipedia/src/components/Input.tsx
+++ 10-nookipedia/src/components/Input.tsx
@@ -27,7 +27,10 @@ export default function Input({ id, value, onChange, onSubmit }: InputProps) {
         placeholder="검색어를 입력해주세요"
         className="border-1 rounded-sm p-2 w-100"
       />
-      <button type="submit" className="border-1 rounded-sm p-2">
+      <button
+        type="submit"
+        className="border-1 rounded-sm py-1 px-4 font-bold text-[1.2rem] text-[#614f34] bg-white"
+      >
         검색
       </button>
     </form>
